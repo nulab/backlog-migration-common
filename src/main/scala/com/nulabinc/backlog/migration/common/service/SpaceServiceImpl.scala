bdc38cdcdb0e7309c77dac9a4879b95b52a17e32
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-05-30 12:12:17 +0900

    Remove: environment

--- src/main/scala/com/nulabinc/backlog/migration/common/service/SpaceServiceImpl.scala
+++ src/main/scala/com/nulabinc/backlog/migration/common/service/SpaceServiceImpl.scala
@@ -3,14 +3,14 @@ package com.nulabinc.backlog.migration.common.service
 import javax.inject.Inject
 
 import com.nulabinc.backlog.migration.common.convert.Convert
-import com.nulabinc.backlog.migration.common.convert.writes.{EnvironmentWrites, SpaceWrites}
-import com.nulabinc.backlog.migration.common.domain.{BacklogEnvironment, BacklogSpace}
+import com.nulabinc.backlog.migration.common.convert.writes.SpaceWrites
+import com.nulabinc.backlog.migration.common.domain.BacklogSpace
 import com.nulabinc.backlog4j.BacklogClient
 
 /**
   * @author uchida
   */
-class SpaceServiceImpl @Inject()(implicit val spaceWrites: SpaceWrites, implicit val environmentWrites: EnvironmentWrites, backlog: BacklogClient)
+class SpaceServiceImpl @Inject()(implicit val spaceWrites: SpaceWrites, implicit val backlog: BacklogClient)
     extends SpaceService {
 
   override def space(): BacklogSpace =
@@ -23,8 +23,5 @@ class SpaceServiceImpl @Inject()(implicit val spaceWrites: SpaceWrites, implicit
     } catch {
       case _: Throwable => false
     }
-
-  override def environment(): BacklogEnvironment =
-    Convert.toBacklog(backlog.getEnvironment)
 
 }
