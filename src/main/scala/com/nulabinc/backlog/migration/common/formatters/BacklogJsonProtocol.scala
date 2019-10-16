2f1d817327cc8849ea6416d61605e637a2ae8d99
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2019-10-16 12:18:25 +0900

    BLGMIGRATION-810 clean: move package

--- src/main/scala/com/nulabinc/backlog/migration/common/formatters/BacklogJsonProtocol.scala
+++ src/main/scala/com/nulabinc/backlog/migration/common/formatters/BacklogJsonProtocol.scala
@@ -1,7 +1,8 @@
-package com.nulabinc.backlog.migration.common.domain
+package com.nulabinc.backlog.migration.common.formatters
 
+import com.nulabinc.backlog.migration.common.domain._
 import com.nulabinc.backlog4j.CustomField.FieldType
-import spray.json.{DefaultJsonProtocol, JsNumber, JsValue, RootJsonFormat, _}
+import spray.json.{DefaultJsonProtocol, JsNumber, JsString, JsValue, RootJsonFormat, _}
 
 import scala.math.BigDecimal
 
@@ -45,7 +46,8 @@ object BacklogJsonProtocol extends DefaultJsonProtocol {
     }
   }
 
-  implicit val BacklogWikiFormat                        = jsonFormat9(BacklogWiki)
+  implicit val BacklogWikiTagFormat                     = jsonFormat2(BacklogWikiTag)
+  implicit val BacklogWikiFormat                        = jsonFormat10(BacklogWiki)
   implicit val BacklogCustomFieldInitialDateFormat      = jsonFormat3(BacklogCustomFieldInitialDate)
   implicit val BacklogCustomFieldTextPropertyFormat     = jsonFormat1(BacklogCustomFieldTextProperty)
   implicit val BacklogCustomFieldNumericPropertyFormat  = jsonFormat5(BacklogCustomFieldNumericProperty)
