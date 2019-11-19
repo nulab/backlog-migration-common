2393dafc7dd6efdbdc400bcb46969a89e97bc447
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2019-11-19 16:52:52 +0900

    add: for reference from importer

---
+++ src/main/scala/com/nulabinc/backlog/migration/common/conf/ExcludeOption.scala
@@ -0,0 +1,14 @@
+package com.nulabinc.backlog.migration.common.conf
+
+case class ExcludeOption(issue: Boolean, wiki: Boolean) {
+  override def toString: String = {
+    val issueArr = if (issue) Seq("issue") else Seq()
+    val wikiArr = if (wiki) Seq("wiki") else Seq()
+
+    (issueArr ++ wikiArr).mkString(", ")
+  }
+}
+
+object ExcludeOption {
+  val default: ExcludeOption = ExcludeOption(issue = false, wiki = false)
+}
