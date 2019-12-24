1c79389858ffd35e405d354bfad4f9e36eecd0ce
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2019-12-24 15:55:52 +0900

    BLGMIGRATION-846 add: methods for storage dsl

--- core/src/main/scala/com/nulabinc/backlog/migration/common/dsl/StorageDsl.scala
+++ core/src/main/scala/com/nulabinc/backlog/migration/common/dsl/StorageDsl.scala
@@ -4,5 +4,12 @@ import java.nio.file.Path
 
 trait StorageDsl[F[_]] {
 
+  def readFile(path: Path): F[String]
+
+  def writeFile(path: Path, content: String): F[Unit]
+
   def exists(path: Path): F[Boolean]
+
+  def delete(path: Path): F[Boolean]
+
 }
