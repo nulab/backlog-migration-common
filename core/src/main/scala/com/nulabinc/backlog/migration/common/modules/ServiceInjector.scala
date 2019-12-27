d59d41353d729bbcea1d247b21be84570376d327
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2019-12-27 10:53:06 +0900

    BLGMIGRATION-855 add: status service in injector

--- core/src/main/scala/com/nulabinc/backlog/migration/common/modules/ServiceInjector.scala
+++ core/src/main/scala/com/nulabinc/backlog/migration/common/modules/ServiceInjector.scala
@@ -3,6 +3,7 @@ package com.nulabinc.backlog.migration.common.modules
 import com.google.inject.{AbstractModule, Guice, Injector}
 import com.nulabinc.backlog.migration.common.client.{BacklogAPIClient, BacklogAPIClientImpl}
 import com.nulabinc.backlog.migration.common.conf.{BacklogApiConfiguration, BacklogPaths}
+import com.nulabinc.backlog.migration.common.domain.BacklogProjectKey
 import com.nulabinc.backlog.migration.common.service._
 import com.nulabinc.backlog4j.conf.BacklogPackageConfigure
 
@@ -18,10 +19,12 @@ object ServiceInjector {
         val configure               = backlogPackageConfigure.apiKey(apiConfig.key)
         val backlog                 = new BacklogAPIClientImpl(configure)
 
+        bind(classOf[BacklogProjectKey]).toInstance(BacklogProjectKey(apiConfig.projectKey))
         bind(classOf[BacklogAPIClient]).toInstance(backlog)
         bind(classOf[ProjectService]).to(classOf[ProjectServiceImpl])
         bind(classOf[SpaceService]).to(classOf[SpaceServiceImpl])
         bind(classOf[UserService]).to(classOf[UserServiceImpl])
+        bind(classOf[StatusService]).to(classOf[StatusServiceImpl])
         bind(classOf[PriorityService]).to(classOf[PriorityServiceImpl])
         bind(classOf[BacklogPaths]).toInstance(new BacklogPaths(apiConfig.projectKey))
       }
