fd67de9ea8225055b48fe5ec1381f05a5fe3208b
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2020-02-03 15:55:33 +0900

    add: base table

--- core/src/main/scala/com/nulabinc/backlog/migration/common/domain/mappings/Mapping.scala
+++ core/src/main/scala/com/nulabinc/backlog/migration/common/domain/mappings/Mapping.scala
@@ -1,7 +1,14 @@
 package com.nulabinc.backlog.migration.common.domain.mappings
 
+import com.nulabinc.backlog.migration.common.domain.Entity
+import com.nulabinc.backlog.migration.common.domain.Types.AnyId
+
 sealed trait Mapping
 
-case class StatusMapping[A](optSrc: Option[A], optDst: Option[BacklogStatusMappingItem]) extends Mapping
+trait StatusMapping[A] extends Mapping with Entity {
+  val id: AnyId
+  val optSrc: Option[A]
+  val optDst: Option[BacklogStatusMappingItem]
+}
 
 case class BacklogStatusMappingItem(value: MappingValue)
