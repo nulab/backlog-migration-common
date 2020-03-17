5ac5df47d927787f84d254aa4d8a6e25a646409a
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2020-03-17 18:50:46 +0900

    BLGMIGRATION-866 add: document

--- core/src/main/scala/com/nulabinc/backlog/migration/common/services/StatusMappingFileService.scala
+++ core/src/main/scala/com/nulabinc/backlog/migration/common/services/StatusMappingFileService.scala
@@ -11,19 +11,25 @@ import com.nulabinc.backlog.migration.common.dsl.{AppDSL, ConsoleDSL, StorageDSL
 import com.nulabinc.backlog.migration.common.errors.{MappingFileError, MappingFileNotFound}
 import com.nulabinc.backlog.migration.common.formatters.Formatter
 import com.nulabinc.backlog.migration.common.serializers.Serializer
-import com.nulabinc.backlog.migration.common.validators.MappingValidator
+import com.nulabinc.backlog.migration.common.validators.{MappingValidator, MappingValidatorNec}
 import org.apache.commons.csv.CSVRecord
 
 private case class MergedStatusMapping[A](mergeList: Seq[StatusMapping[A]], addedList: Seq[StatusMapping[A]])
 
 private object MergedStatusMapping {
-  def empty[A]: MergedStatusMapping[A] = MergedStatusMapping[A](mergeList = Seq(), addedList = Seq())
+  def empty[A]: MergedStatusMapping[A] =
+    MergedStatusMapping[A](mergeList = Seq(), addedList = Seq())
 }
 
 object StatusMappingFileService {
   import com.nulabinc.backlog.migration.common.messages.ConsoleMessages.{Mappings => MappingMessages}
   import com.nulabinc.backlog.migration.common.shared.syntax._
 
+  /**
+    Create mapping files.
+      - statuses.csv       Link the source and destination states. User must edit to link it.
+      - statuses_list.csv  List of items that can be specified in statuses.csv
+   */
   def init[A, F[_]: Monad: StorageDSL: ConsoleDSL](mappingFilePath: Path,
                                                    mappingListPath: Path,
                                                    srcItems: Seq[A],
@@ -57,6 +63,17 @@ object StatusMappingFileService {
       _ <- StorageDSL[F].writeNewFile(mappingListPath, MappingSerializer.statusList(dstItems))
     } yield ()
 
+  /**
+   * List of items that can be specified in statuses.csv
+   *
+   * @param path
+   * @param dstItems
+   * @param deserializer
+   * @param validator
+   * @tparam A
+   * @tparam F
+   * @return
+   */
   def execute[A, F[_]: Monad: AppDSL: StorageDSL: ConsoleDSL](path: Path, dstItems: BacklogStatuses)
                                                              (implicit deserializer: Deserializer[CSVRecord, StatusMapping[A]],
                                                               validator: MappingValidator[StatusMapping[A]]): F[Either[MappingFileError, IndexedSeq[ValidatedStatusMapping[A]]]] =
@@ -72,6 +89,35 @@ object StatusMappingFileService {
 ???
     }
 
+  /**
+   * Merge old mappings and new items.
+   *
+   * @param mappings
+   * @param srcItems
+   * @tparam A
+   * @return
+   */
+  private def merge[A](mappings: Seq[StatusMapping[A]], srcItems: Seq[A]): MergedStatusMapping[A] =
+    srcItems.foldLeft(MergedStatusMapping.empty[A]) { (acc, item) =>
+      mappings.find(_.src == item) match {
+        case Some(value) =>
+          acc.copy(mergeList = acc.mergeList :+ value)
+        case None =>
+          val mapping = StatusMapping.create(item)
+          acc.copy(mergeList = acc.mergeList :+ mapping, addedList = acc.addedList :+ mapping)
+      }
+    }
+
+  /**
+   * Deserialize a mapping file.
+   *
+   * @param exists
+   * @param path
+   * @param deserializer
+   * @tparam A
+   * @tparam F
+   * @return
+   */
   private def getMapping[A, F[_]: Monad: ConsoleDSL: StorageDSL](exists: Boolean, path: Path)
                                                                 (implicit deserializer: Deserializer[CSVRecord, StatusMapping[A]]): F[Either[MappingFileError, IndexedSeq[StatusMapping[A]]]] = {
     val res: F[Either[MappingFileError, IndexedSeq[StatusMapping[A]]]] = if (!exists) {
@@ -88,18 +134,12 @@ object StatusMappingFileService {
     res.handleError.value
   }
 
-  private def merge[A](mappings: Seq[StatusMapping[A]], srcItems: Seq[A]): MergedStatusMapping[A] =
-    srcItems.foldLeft(MergedStatusMapping.empty[A]) { (acc, item) =>
-      mappings.find(_.src == item) match {
-        case Some(value) =>
-          acc.copy(mergeList = acc.mergeList :+ value)
-        case None =>
-          val mapping = StatusMapping.create(item)
-          acc.copy(mergeList = acc.mergeList :+ mapping, addedList = acc.addedList :+ mapping)
-      }
-    }
 
-  private def validate[A](mappings: Seq[StatusMapping[A]]): Either[MappingFileError, Seq[ValidatedStatusMapping[A]]] = ???
+  private def validate[A](mappings: Seq[StatusMapping[A]], dstItems: BacklogStatuses)
+                         (implicit validator: MappingValidator[A]): Either[MappingFileError, Seq[ValidatedStatusMapping[A]]] = {
+//    mappings.map(MappingValidatorNec.validateStatusMapping(_, dstItems))
+    ???
+  }
 
 }
 
