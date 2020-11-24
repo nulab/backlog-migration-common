6fe7981277117d5debbfa151879fcf550ac7e304
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2020-11-24 10:08:37 +0900

    change: db library slick to doobie

--- core/src/main/scala/com/nulabinc/backlog/migration/common/dsl/StatusMappingStoreDSL.scala
+++ core/src/main/scala/com/nulabinc/backlog/migration/common/dsl/StatusMappingStoreDSL.scala
@@ -1,19 +1,19 @@
 package com.nulabinc.backlog.migration.common.dsl
 
-import com.nulabinc.backlog.migration.common.domain.mappings.StatusMapping
-import com.nulabinc.backlog.migration.common.persistence.sqlite.DBIOTypes.{DBIORead, DBIOWrite}
+// import com.nulabinc.backlog.migration.common.domain.mappings.StatusMapping
+// import com.nulabinc.backlog.migration.common.persistence.sqlite.DBIOTypes.{DBIORead, DBIOWrite}
 
-trait StatusMappingQuery[A] {
-  def findQuery(mapping: StatusMapping[A]): DBIORead[Option[StatusMapping[A]]]
-  def saveQuery(mapping: StatusMapping[A]): DBIOWrite
-}
+// trait StatusMappingQuery[A] {
+//   def findQuery(mapping: StatusMapping[A]): DBIORead[Option[StatusMapping[A]]]
+//   def saveQuery(mapping: StatusMapping[A]): DBIOWrite
+// }
 
-trait StatusMappingStoreDSL[F[_], A] {
+// trait StatusMappingStoreDSL[F[_], A] {
 
-  val storeDSL: StoreDSL[F]
+//   val storeDSL: StoreDSL[F]
 
-  implicit val mq: StatusMappingQuery[A]
+//   implicit val mq: StatusMappingQuery[A]
 
-  def save(mapping: StatusMapping[A]): F[Int] =
-    storeDSL.write(mq.saveQuery(mapping))
-}
+//   def save(mapping: StatusMapping[A]): F[Int] =
+//     storeDSL.write(mq.saveQuery(mapping))
+// }
--- core/src/main/scala/com/nulabinc/backlog/migration/common/persistence/sqlite/ops/AllTableOps.scala
+++ core/src/main/scala/com/nulabinc/backlog/migration/common/persistence/sqlite/ops/AllTableOps.scala
@@ -1,13 +1,12 @@
 package com.nulabinc.backlog.migration.common.persistence.sqlite.ops
 
-import monix.execution.Scheduler
-import slick.jdbc.SQLiteProfile.api._
+// import monix.execution.Scheduler
 
-case class AllTableOps()(implicit exc: Scheduler) {
-  val backlogStatusTableOps = StatusTableOps()
+// case class AllTableOps()(implicit exc: Scheduler) {
+//   val backlogStatusTableOps = StatusTableOps()
 
-  val createDatabaseOps =
-    DBIO.seq(
-      backlogStatusTableOps.createTable
-    )
-}
+//   val createDatabaseOps =
+//     DBIO.seq(
+//       backlogStatusTableOps.createTable
+//     )
+// }
