646584a0e37ce1d7574115388e48f35a34105c87
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2020-12-15 20:03:26 +0900

    BLGMIGRATION-922 change: define typeclss

--- core/src/main/scala/com/nulabinc/backlog/migration/common/dsl/StoreDSL.scala
+++ core/src/main/scala/com/nulabinc/backlog/migration/common/dsl/StoreDSL.scala
@@ -2,7 +2,9 @@ package com.nulabinc.backlog.migration.common.dsl
 
 import com.nulabinc.backlog.migration.common.domain.BacklogStatus
 import com.nulabinc.backlog.migration.common.domain.BacklogStatuses
+import simulacrum.typeclass
 
+@typeclass
 trait StoreDSL[F[_]] {
   def findBacklogStatus(id: Int): F[Option[BacklogStatus]]
   def storeBacklogStatus(status: BacklogStatus): F[Unit]
