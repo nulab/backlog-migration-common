d57854b4e439087c1f49c2c09ce929d87128bb20
saki <sakihet@gmail.com>
2026-08-18 09:40:53 +0900

    feat: add Document import support to BacklogAPIClient and DocumentService

--- core/src/main/scala/com/nulabinc/backlog/migration/common/service/DocumentService.scala
+++ core/src/main/scala/com/nulabinc/backlog/migration/common/service/DocumentService.scala
@@ -2,7 +2,13 @@ package com.nulabinc.backlog.migration.common.service
 
 import java.io.InputStream
 
-import com.nulabinc.backlog.migration.common.domain.{BacklogDocument, BacklogDocumentTree}
+import com.nulabinc.backlog.migration.common.domain.{
+  BacklogAttachment,
+  BacklogDocument,
+  BacklogDocumentComment,
+  BacklogDocumentTag,
+  BacklogDocumentTree
+}
 
 /**
  * @author
@@ -22,5 +28,35 @@ trait DocumentService {
       documentId: String,
       attachmentId: Long
   ): Option[(String, InputStream)]
+
+  def create(
+      projectId: Long,
+      document: BacklogDocument,
+      optParentId: Option[String],
+      addLast: Boolean,
+      propertyResolver: PropertyResolver
+  ): String
+
+  def updateContent(
+      documentId: String,
+      document: BacklogDocument,
+      propertyResolver: PropertyResolver
+  ): Unit
+
+  def addComment(
+      documentId: String,
+      comment: BacklogDocumentComment,
+      propertyResolver: PropertyResolver
+  ): Either[Throwable, String]
+
+  def addAttachment(
+      documentId: String,
+      path: String
+  ): Either[Throwable, BacklogAttachment]
+
+  def addTags(
+      documentId: String,
+      tagNames: Seq[String]
+  ): Either[Throwable, Seq[BacklogDocumentTag]]
 
 }
---
+++ core/src/test/scala/com/nulabinc/backlog/migration/service/DocumentServiceImplSpec.scala
@@ -0,0 +1,173 @@
+package com.nulabinc.backlog.migration.service
+
+import com.google.inject.Guice
+import com.nulabinc.backlog.migration.common.conf.BacklogApiConfiguration
+import com.nulabinc.backlog.migration.common.domain.{
+  BacklogDocument,
+  BacklogDocumentComment,
+  BacklogDocumentCommentReply
+}
+import com.nulabinc.backlog.migration.common.modules.DefaultModule
+import com.nulabinc.backlog.migration.common.service.DocumentServiceImpl
+import com.nulabinc.backlog.migration.{SimpleFixture, TestPropertyResolver}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import spray.json._
+
+/**
+ * @author
+ *   nulab
+ */
+class DocumentServiceImplSpec extends AnyFlatSpec with Matchers with SimpleFixture {
+
+  def documentService(): DocumentServiceImpl =
+    Guice
+      .createInjector(
+        new DefaultModule(BacklogApiConfiguration("url", "key", "projectKey"))
+      )
+      .getInstance(classOf[DocumentServiceImpl])
+
+  private val documentCreated = "2015-05-01T16:01:51+09:00"
+  private val documentUpdated = "2015-05-02T16:01:51+09:00"
+
+  private val document = BacklogDocument(
+    optId = None,
+    projectId = projectId,
+    title = "document title",
+    optJson = Some("""{"type":"doc","content":[]}"""),
+    optPlain = Some("plain text"),
+    optEmoji = Some(":smile:"),
+    tags = Seq.empty,
+    attachments = Seq.empty,
+    comments = Seq.empty,
+    optCreatedUser = Some(user1),
+    optCreated = Some(documentCreated),
+    optUpdatedUser = Some(user2),
+    optUpdated = Some(documentUpdated)
+  )
+
+  "createDocumentJson" should "build the import request body" in {
+    val propertyResolver = new TestPropertyResolver()
+
+    val json = documentService().createDocumentJson(
+      projectId,
+      document,
+      Some("parentDocumentId"),
+      addLast = true,
+      propertyResolver
+    )
+
+    json.fields("projectId") should be(JsNumber(projectId))
+    json.fields("title") should be(JsString("document title"))
+    json.fields("emoji") should be(JsString(":smile:"))
+    json.fields("parentId") should be(JsString("parentDocumentId"))
+    json.fields("addLast") should be(JsBoolean(true))
+    json.fields("created") should be(JsString(documentCreated))
+    json.fields("createdUserId") should be(JsNumber(userId1))
+    json.fields("updated") should be(JsString(documentUpdated))
+    json.fields("updatedUserId") should be(JsNumber(userId2))
+    json.fields.keySet should not contain "content"
+  }
+
+  it should "omit optional fields that are not set" in {
+    val propertyResolver = new TestPropertyResolver()
+    val minimalDocument = document.copy(
+      optEmoji = None,
+      optCreatedUser = None,
+      optCreated = None,
+      optUpdatedUser = None,
+      optUpdated = None
+    )
+
+    val json = documentService().createDocumentJson(
+      projectId,
+      minimalDocument,
+      None,
+      addLast = false,
+      propertyResolver
+    )
+
+    json.fields.keySet should contain theSameElementsAs Set("projectId", "title", "addLast")
+  }
+
+  "updateContentJson" should "build the content import request body" in {
+    val propertyResolver = new TestPropertyResolver()
+
+    val json = documentService().updateContentJson(document, propertyResolver)
+
+    json.fields("json") should be("""{"type":"doc","content":[]}""".parseJson)
+    json.fields("plain") should be(JsString("plain text"))
+    json.fields("updated") should be(JsString(documentUpdated))
+    json.fields("updatedUserId") should be(JsNumber(userId2))
+    json.fields.keySet should not contain "title"
+  }
+
+  it should "fall back to an empty object and empty string when content is missing" in {
+    val propertyResolver = new TestPropertyResolver()
+    val emptyDocument     = document.copy(optJson = None, optPlain = None)
+
+    val json = documentService().updateContentJson(emptyDocument, propertyResolver)
+
+    json.fields("json") should be(JsObject.empty)
+    json.fields("plain") should be(JsString(""))
+  }
+
+  "commentJson" should "build the comment import request body with replies" in {
+    val propertyResolver = new TestPropertyResolver()
+    val reply = BacklogDocumentCommentReply(
+      optId = None,
+      content = "reply content",
+      plain = "reply plain",
+      optCreatedUser = Some(user3),
+      optCreated = Some(documentCreated),
+      optUpdated = None
+    )
+    val comment = BacklogDocumentComment(
+      optId = None,
+      statusId = 1,
+      content = "comment content",
+      plain = "comment plain",
+      commentType = "comment",
+      optCreatedUser = Some(user1),
+      optCreated = Some(documentCreated),
+      optUpdated = Some(documentUpdated),
+      replies = Seq(reply)
+    )
+
+    val json = documentService().commentJson(comment, propertyResolver)
+
+    json.fields("content") should be(JsString("comment content"))
+    json.fields("plain") should be(JsString("comment plain"))
+    json.fields("statusId") should be(JsNumber(1))
+    json.fields("commentType") should be(JsString("comment"))
+    json.fields("createdUserId") should be(JsNumber(userId1))
+
+    val replies = json.fields("replies").asInstanceOf[JsArray].elements
+    replies should have size 1
+    val replyJson = replies.head.asJsObject
+    replyJson.fields("content") should be(JsString("reply content"))
+    replyJson.fields("plain") should be(JsString("reply plain"))
+    replyJson.fields("createdUserId") should be(JsNumber(userId3))
+    replyJson.fields.keySet should not contain "updatedUserId"
+  }
+
+  it should "omit the replies field when there are no replies" in {
+    val propertyResolver = new TestPropertyResolver()
+    val comment = BacklogDocumentComment(
+      optId = None,
+      statusId = 1,
+      content = "comment content",
+      plain = "comment plain",
+      commentType = "comment",
+      optCreatedUser = None,
+      optCreated = None,
+      optUpdated = None,
+      replies = Seq.empty
+    )
+
+    val json = documentService().commentJson(comment, propertyResolver)
+
+    json.fields.keySet should not contain "replies"
+  }
+
+}
